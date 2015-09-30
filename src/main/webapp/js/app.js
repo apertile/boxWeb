c77b4bee2c85a264f30af72b1cac8360ffa1eddf
iarielduarte <iariel.javauser@gmail.com>
2015-09-30 01:06:18 -0300

    Header and content no more overlapped

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -19,3 +19,14 @@ boxApp.config(['$routeProvider',
                    			});
                    	}
                    ]);
+
+boxApp.directive('header', function () {
+    return {
+        restrict: 'A', //This menas that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
+        replace: true,
+        templateUrl: 'partials/header.html',
+        controller: ['$scope', '$filter', function ($scope, $filter) {
+            // Your behaviour goes here :)
+        }]
+    }
+});
